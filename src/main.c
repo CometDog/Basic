13ed61826b6eeb6fb2397205adabd2a850fa01d0
James <james.j.downs@gmail.com>
2015-05-24 12:04:11 -0400

    Fixed some bugs

--- src/main.c
+++ src/main.c
@@ -53,17 +53,17 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
   APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
-static void update_battery(Layer* layer, GContext *ctx) {
+static void update_time(Layer *layer, GContext *ctx) {
+	time_t epoch = time(NULL);
+	struct tm *t = localtime(&epoch);
+	
 	bat = battery_state_service_peek().charge_percent / 10;
 	#ifdef PBL_COLOR
 		if (bat >= 7) {
 			window_set_background_color(s_main_window, GColorIslamicGreen);
 		}
 		else if (bat >= 4) {
-			window_set_background_color(s_main_window, GColorYellow);
-			text_layer_set_colors(s_weather_label, GColorBlack, GColorClear);
-			text_layer_set_colors(s_time_label, GColorBlack, GColorClear);
-			text_layer_set_colors(s_date_label, GColorBlack, GColorClear);
+			window_set_background_color(s_main_window, GColorOrange);
 		}
 		else {
 			window_set_background_color(s_main_window, GColorDarkCandyAppleRed);
@@ -72,11 +72,6 @@ static void update_battery(Layer* layer, GContext *ctx) {
 		graphics_context_set_fill_color(ctx, GColorWhite);
 		graphics_fill_rect(ctx, GRect(0,163,bat * 14.4, 5), GCornerNone, 0);
 	#endif
-}
-
-static void update_time(Layer *layer, GContext *ctx) {
-	time_t epoch = time(NULL);
-	struct tm *t = localtime(&epoch);
 	
 	if (clock_is_24h_style() == true) {
 		print_time(s_time_buffer, "%H:%M", t);
@@ -88,8 +83,6 @@ static void update_time(Layer *layer, GContext *ctx) {
 	
 	text_layer_set_text(s_time_label, s_time_buffer);
 	text_layer_set_text(s_date_label, s_date_buffer);
-	
-	layer_set_update_proc(s_info_layer, update_battery);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
@@ -129,10 +122,7 @@ static void main_window_load(Window *window) {
 			window_set_background_color(s_main_window, GColorIslamicGreen);
 		}
 		else if (bat >= 4) {
-			window_set_background_color(s_main_window, GColorYellow);
-			text_layer_set_colors(s_weather_label, GColorBlack, GColorClear);
-			text_layer_set_colors(s_time_label, GColorBlack, GColorClear);
-			text_layer_set_colors(s_date_label, GColorBlack, GColorClear);
+			window_set_background_color(s_main_window, GColorOrange);
 		}
 		else {
 			window_set_background_color(s_main_window, GColorDarkCandyAppleRed);
@@ -148,8 +138,7 @@ static void main_window_load(Window *window) {
 	text_layer_set_text_alignment(s_weather_label, GTextAlignmentCenter);
 	text_layer_set_text_alignment(s_time_label, GTextAlignmentCenter);
 	text_layer_set_text_alignment(s_date_label, GTextAlignmentCenter);
-	
-	layer_set_update_proc(s_info_layer, update_battery);
+
 	layer_set_update_proc(s_info_layer, update_time);
 	
 	layer_add_to_window(s_info_layer, window);
